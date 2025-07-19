6118e517fac08ae1ac9d9b7b6375c7231c64680a
Lee Yunjin <gzblues61@daum.net>
2025-07-20 01:11:03 +0900

    trying hash router

--- blog-frontend/src/index.js
+++ blog-frontend/src/index.js
@@ -1,14 +1,24 @@
+// ~/chi-blog/blog-frontend/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Global styling for the application
-import App from './App'; // The root component of the application
+// Import HashRouter instead of BrowserRouter
+import { HashRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// Create a root to render the React application
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// Render the App component inside React.StrictMode for development checks
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Use HashRouter for GitHub Pages compatibility */}
+    {/* HashRouter does not require 'basename' as it uses URL hashes */}
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
